9a85106567cd7abf75a801b4d6a770eddb382233
Robert Andrei Parasca <Robert.Parasca@endava.com>
2019-10-24 17:41:10 +0300

    Added /me endpoint

--- routes/api.php
+++ routes/api.php
@@ -13,16 +13,11 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::middleware(['auth:api'])->group(function () {
     Route::get('/me', function (Request $request) {
         return $request->user();
     });
 });
-
 
 Route::post('/register', 'Api\AuthController@register');
 Route::post('/login-password', 'Api\AuthController@login');
